a3399c1684c24bb4d3cbb2153b079402ea229abf
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-07-17 11:02:19 -0700

    added and pass get all, put, and delete for show

--- test/show.test.js
+++ test/show.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Show = require('../lib/models/Show');
 
 describe('show routes', () => {
   beforeAll(() => {
@@ -26,6 +27,66 @@ describe('show routes', () => {
           title: 'hannibal',
           seasons: 4,
           channel: 'nbc',
+          __v: 0
+        });
+      });
+  });
+
+  it('get all shows', async() => {
+    const show = await Show.create({ title: 'hannibal', seasons: 4, channel: 'nbc' });
+    return request(app)
+      .get('/api/v1/shows')
+      .then(res => {
+        const showJSON = JSON.parse(JSON.stringify(show));
+        expect(res.body).toEqual([showJSON]);
+      });
+  });
+
+  it('get show by id', async() => {
+    const show = await Show.create({ title: 'hannibal', seasons: 4, channel: 'nbc'});
+    return request(app)
+      .get(`/api/v1/shows/${show._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          title: 'hannibal',
+          seasons: 4,
+          channel: 'nbc',
+          __v: 0
+        });
+      });
+  });
+
+  it('put show', async() => {
+    const show = await Show.create({ title: 'hannibal', seasons: 4, channel: 'nbc' });
+    return request(app)
+      .put(`/api/v1/shows/${show._id}`)
+      .send({
+        title: 'dark',
+        seasons: 2,
+        channel: 'netflix'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          title: 'dark',
+          seasons: 2,
+          channel: 'netflix',
+          __v: 0
+        });
+      });
+  });
+
+  it('deltes show', async() => {
+    const show = await Show.create({ title: 'game of thrones', seasons: 8, channel: 'hbo' });
+    return request(app)
+      .delete(`/api/v1/shows/${show._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          title: 'game of thrones',
+          seasons: 8,
+          channel: 'hbo',
           __v: 0
         });
       });
